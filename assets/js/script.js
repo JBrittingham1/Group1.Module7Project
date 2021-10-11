1af783bfadd70228ef6e1b3bacc1ac284b4a28a3
JBGinger <jeff.brittingham@protonmail.com>
2021-10-10 22:10:10 -0400

    Set link to Ron's API to https instead of http

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 const kwApi = 'https://api.kanye.rest/';
-const rsApi = 'http://ron-swanson-quotes.herokuapp.com/v2/quotes';
+const rsApi = 'https://ron-swanson-quotes.herokuapp.com/v2/quotes';
 const kwGif = 'https://api.giphy.com/v1/gifs/search?q=kanye-west&api_key=p8XQjyndOC4ycMWcXs2q7C3OCZks2J92';
 const rsGif = 'https://api.giphy.com/v1/gifs/search?q=ron-swanson&api_key=p8XQjyndOC4ycMWcXs2q7C3OCZks2J92';
 const clearScoresBtn = document.querySelector('#clear-scores');
